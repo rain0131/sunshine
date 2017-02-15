af0f9790cb6c33bbf06b1525b204a32a267466a7
rain0749 <545943830@qq.com>
2017-02-15 13:13:57 +0800

    11

---
+++ test1.py
@@ -0,0 +1,16 @@
+# encoding: utf-8
+#!/usr/bin/python
+
+import string
+import unittest
+import selenium
+from selenium import webdriver
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+import os
+from selenium.webdriver.common.keys import Keys
+
+import unittest
+
+
+print 'hello !!'
